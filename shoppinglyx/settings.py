80425c8c06e9e9587c6998820f87cc6b2ee4ab1f
chinmaya943 <110168047+chinmaya943@users.noreply.github.com>
2022-11-06 13:21:49 +0530

    proc/setting commit

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -29,11 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8o353)z(_*^04+jhey!m*q3_no@vhn*7v8(6xz4rl6=kunu=it'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["vdm12345.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["thegroceryhouse.herokuapp.com", "localhost"]
 
 
 # Application definition
